94effe849e7c08e338734f61b7ec41caefdaebb2
hy949 <1661346771@qq.com>
2019-09-30 01:17:20 +0800

    添加Karma|Chai|Mocha

---
+++ test/button.test.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import Button from '../src/button'
+const expect = chai.expect;
+
+Vue.config.productionTip = false;
+Vue.config.devtools = false;
+
+describe('Button', () => {
+  it('存在.', () => {
+    expect(Button).to.be.ok;
+  });
+});
