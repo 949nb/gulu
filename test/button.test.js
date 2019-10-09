5ac7de9f728d6cfaf683c985c77b51af8d9764f5
hy949 <1661346771@qq.com>
2019-10-09 17:09:01 +0800

    注释button的测试代码

--- test/button.test.js
+++ test/button.test.js
@@ -1,15 +1,15 @@
-import Vue from 'vue';
-import * as chai from 'chai';
-import Button from '../src/button';
+// import Vue from 'vue';
+// import * as chai from 'chai';
+// import Button from '../src/button';
 
 
-const expect = chai.assert;
+// const expect = chai.assert;
 
-Vue.config.productionTip = false;
-Vue.config.devtools = false;
+// Vue.config.productionTip = false;
+// Vue.config.devtools = false;
 
-describe('Button', () => {
-  it('存在.', () => {
-    expect(Button);
-  });
-});
+// describe('Button', () => {
+//   it('存在.', () => {
+//     expect(Button);
+//   });
+// });
